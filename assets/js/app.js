9171c658e144a25011cd8449d5be8f123221c56c
mickequeen <mickequeen@gmail.com>
2018-02-15 23:41:46 -0300

    añadiendo estilos e info a modal y tarjetas de cada pokémon

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
  $.ajax({
-   url: 'https://pokeapi.co/api/v2/pokemon/',
+   url: 'https://pokeapi.co/api/v2/pokemon/?limit=150',
    type: 'GET',
    success: function (results) {
      //console.log(results.sprites.front_default);
@@ -10,7 +10,7 @@ $(document).ready(function(){
          url: url,
          type: 'GET',
          success: function (data) {
-          var imgPokemon = $('<div class="panel panel-default" id="panel_' + data.id + '"><div class="panel-body"><a data-toggle="modal" href="#myModal"><button id= "button_' + data.id + '"><img src=' + data.sprites.front_default +' id=' + data.id + '></button></a></div><p>' + data.name + '</p></div>');
+          var imgPokemon = $('<div class="panel panel-default" id="panel_' + data.id + '"><div class="panel-body"><a data-toggle="modal" href="#myModal"><button class="touch" id= "button_' + data.id + '"><img src=' + data.sprites.front_default +' id=' + data.id + '></button></a></div><p>' + data.name + '</p></div>');
           $('.pokemon').append(imgPokemon);
           /*
           *colorear según tipo
@@ -19,14 +19,49 @@ $(document).ready(function(){
             $('#panel_' + data.id).addClass('fireColor')
           } if (data.types[0].type.name === 'water') {
             $('#panel_' + data.id).addClass('waterColor')
-          }
-          $('#button_'+ data.id).click(function(){
-            $('.modal-title , #type , #secondType').empty();
+          } if (data.types[0].type.name === 'normal') {
+            $('#panel_' + data.id).addClass('normalColor')
+          } if (data.types[0].type.name === 'poison') {
+            $('#panel_' + data.id).addClass('poisonColor')
+          } if (data.types[0].type.name === 'fairy') {
+            $('#panel_' + data.id).addClass('fairyColor')
+          } if (data.types[0].type.name === 'flying') {
+            $('#panel_' + data.id).addClass('flyingColor')
+          } if (data.types[0].type.name === 'grass') {
+            $('#panel_' + data.id).addClass('grassColor')
+          } if (data.types[0].type.name === 'ground') {
+            $('#panel_' + data.id).addClass('groundColor')
+          } if (data.types[0].type.name === 'electric') {
+            $('#panel_' + data.id).addClass('electricColor')
+          } if (data.types[0].type.name === 'fighting') {
+            $('#panel_' + data.id).addClass('fightingColor')
+          } if (data.types[0].type.name === 'psychic') {
+            $('#panel_' + data.id).addClass('psychicColor')
+          } 
+          /*
+          *función al clickear pokemons (llenado de modal)
+          */
+          $('#panel_'+ data.id).click(function(){
+            /*
+            *limpiar datos creados en modal
+            */
+            $('.modal-title , #type , #secondType , #number, #ability').empty();
             $('#title').append(data.name);
-            if (data.types[1] == undefined) {
-            $('#type').append('Tipo: ' + data.types[0].type.name);
+            $('#number').append('N° Pokédex: #' + data.id);
+            /*
+            *mostrar habilidades
+            */
+            if (data.abilities[1] == undefined) {
+              $('#ability').append('Ability: ' + data.abilities[0].ability.name);
+            } if (data.abilities[2] == undefined) {
+              $('#ability').append('Abilities: ' + data.abilities[0].ability.name + ', ' + data.abilities[1].ability.name) ;
             } else {
-              $('#type').append('Tipo: ' + data.types[0].type.name + '/' + data.types[1].type.name)
+              $('#ability').append('Abilities: ' + data.abilities[0].ability.name + ', ' + data.abilities[1].ability.name + ', ' + data.abilities[2].ability.name)
+            }
+            if (data.types[1] == undefined) {
+            $('#type').append('Type: ' + data.types[0].type.name);
+            } else {
+              $('#type').append('Type: ' + data.types[0].type.name + '/' + data.types[1].type.name)
             }
            })
 
